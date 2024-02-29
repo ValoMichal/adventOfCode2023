6dff77901d5dc5306cef5ac6b44bd1304a501c37
ValoMichal <mvalowork@gmail.com>
2024-02-29 14:49:38 +0100

    d3p1 almost done

--- Day03/part1.c
+++ Day03/part1.c
@@ -1,75 +1,79 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #define size 256
 int main(void){
-	int red = 12;
-	int green = 13;
-	int blue = 14;
 	FILE *input;
 	input=fopen("input.txt","r");
-	int num,sum=0;
-	char signal[size]="";
+	int sum=0;
+	char num[5]="0",prev[size]="",signal[size]="";
 	int j=0;//debug
+	fgets(signal,size,input);
+	fgets(prev,size,input);
 	while(fgets(signal,size,input)){
-		num=-1;
-		for(int i = strlen(signal); i>0; i--){
-			if(i<8){
-				num=-1;
-				if(signal[7]!=':'&&signal[7]!=' '){
-					num=(int)signal[7]-48;
+		for(int i=0;i<strlen(signal);i++){//!!! add limit to array calls with +j so they dont go out of bound (just add if i+j>size)
+			if(prev[i]!='.'&&(prev[i]<'0'||prev[i]>'9')&&((signal[i-1]>='0'&&signal[i-1]<='9')||(signal[i]>='0'&&signal[i]<='9')||(signal[i+1]>='0'&&signal[i+1]<='9'))){//check numbers above
+				for(int j=-3;j<=3;j++){
+					if(signal[i+j]>='0'&&signal[i+j]<='9'){
+						strcat(num,signal[i+j]);
+					}else if(j<0){
+						strcpy(num,"0");
+					}else if(j==0&&signal[i+j+1]>='0'&&signal[i+j+1]<='9'){
+						sum+=atoi(num);
+						strcpy(num,"0");
+					}
 				}
-				if(signal[6]!=':'&&signal[6]!=' '){
-					if(num!=-1){
-						num+=((int)signal[6]-48)*10;
+				sum+=atoi(num);
+				strcpy(num,"0");
+			}
+			if(signal[i]!='.'&&(signal[i]<'0'||signal[i]>'9')&&((prev[i-1]>='0'&&prev[i-1]<='9')||(prev[i]>='0'&&prev[i]<='9')||(prev[i+1]>='0'&&prev[i+1]<='9'))){//check numbers bellow
+				for(int j=-3;j<=3;j++){
+					if(prev[i+j]>='0'&&prev[i+j]<='9'){
+						strcat(num,prev[i+j]);
+					}else if(j<0){
+						strcpy(num,"0");
+					}else if(j==0&&prev[i+j+1]>='0'&&prev[i+j+1]<='9'){
+						sum+=atoi(num);
+						strcpy(num,"0");
+					}
+				}
+				sum+=atoi(num);
+				strcpy(num,"0");
+			}
+			if(signal[i]!='.'&&(signal[i]<'0'||signal[i]>'9')&&(signal[i-1]>='0'&&signal[i-1]<='9')){//check numbers on left
+				for(int j=-3;j<0;j++){
+					if(prev[i+j]>='0'&&prev[i+j]<='9'){
+						strcat(num,prev[i+j]);
 					}else{
-						num=(int)signal[6]-48;
+						strcpy(num,"0");
 					}
 				}
-				if(num==-1){
-					num=(int)signal[5]-48;
-				}else if(signal[7]!=':'&&signal[7]!=' '){
-					num+=((int)signal[5]-48)*100;
-				}else{
-					num+=((int)signal[5]-48)*10;
-				}
-			}else if(signal[i]==' '){
-				if(signal[i+1]=='r'){
-					num=(int)signal[i-1]-48;
-					if(signal[i-2]!=' '){
-						num+=((int)signal[i-2]-48)*10;
-					}
-					if(num>red){
-						num=0;
-						break;	
-					}
-				}else if(signal[i+1]=='g'){
-					num=(int)signal[i-1]-48;
-					if(signal[i-2]!=' '){
-						num+=((int)signal[i-2]-48)*10;
-					}
-					if(num>green){
-						num=0;
-						break;
-					}
-				}else if(signal[i+1]=='b'){
-					num=(int)signal[i-1]-48;
-					if(signal[i-2]!=' '){
-						num+=((int)signal[i-2]-48)*10;
-					}
-					if(num>blue){
-						num=0;
+				sum+=atoi(num);
+				strcpy(num,"0");
+			}
+			if(signal[i]!='.'&&(signal[i]<'0'||signal[i]>'9')&&(signal[i+1]>='0'&&signal[i+1]<='9')){//check numbers on right
+				for(int j=1;j>=3;j++){
+					if(prev[i+j]>='0'&&prev[i+j]<='9'){
+						strcat(num,prev[i+j]);
+					}else{
 						break;
 					}
 				}
+				sum+=atoi(num);
+				strcpy(num,"0");
 			}
 		}
-		sum+=num;
-		
-		if(j>90){//debug
-			printf("%d %d ",sum,num);//debug
+		strcpy(prev,signal);//copy old line before getting new line
+
+		if(j<10){//debug
+			printf("%d ",sum);//debug
 			printf("%s",signal);//debug
 		}//debug
 		j++;//debug
 	}
 	printf("%d\n",sum);
 }
+//first line, find symbols. check if number above line. check if numbers on line. read new line. first line (now second) check if numbers above. repeat
+//read first line outside loop, then override inside loop every time before reading new line
+//
+//get lower line and start loop of reading upper line, matching numbers from upper to lower, from lower to upper and from lower to lower line and then move upper line to lower line and repeat
